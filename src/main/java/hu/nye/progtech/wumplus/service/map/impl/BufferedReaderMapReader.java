b71a649cbab6fdbc08e2513a38a603e891f53dd7
gygyuszi0 <gyorgyigyula8@gmail.com>
2023-11-07 17:54:24 +0100

    MapReader implementáció

---
+++ src/main/java/hu/nye/progtech/wumplus/service/map/impl/BufferedReaderMapReader.java
@@ -0,0 +1,35 @@
+package hu.nye.progtech.wumplus.service.map.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.nye.progtech.wumplus.service.exception.MapReadException;
+import hu.nye.progtech.wumplus.service.map.MapReader;
+
+public class BufferedReaderMapReader implements MapReader {
+
+    private BufferedReader bufferedReader;
+
+    public BufferedReaderMapReader(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
+    }
+
+    @Override
+    public List<String> readMap() throws MapReadException {
+        String line;
+        List<String> result = new ArrayList<>();
+
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (IOException e) {
+            throw new MapReadException("Failed to read map");
+        }
+
+        return result;
+    }
+
+}
