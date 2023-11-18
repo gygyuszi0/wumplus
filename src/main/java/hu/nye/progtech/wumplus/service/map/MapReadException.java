b9773f3189343f9d58ead5704e1af43bd0229dc6
gygyuszi0 <gyorgyigyula8@gmail.com>
2023-11-18 12:01:44 +0100

    Felesleeges fileok törlése

--- src/main/java/hu/nye/progtech/wumplus/service/map/MapReadException.java
+++
@@ -1,9 +0,0 @@
-package hu.nye.progtech.wumplus.service.map;
-
-/**
- * Mep olvasása közben előforduló hibalehetőségek.
- */
-public class MapReadException extends Throwable {
-    public MapReadException(String message) {
-    }
-}
