644f14c6c83949463c5e5fe540079b67c86c3eba
gygyuszi0 <gyorgyigyula8@gmail.com>
2023-11-20 18:52:40 +0100

    StepCommand process és teszt, még nem tökéletes

--- src/main/java/hu/nye/progtech/wumplus/service/command/impl/StepCommand.java
+++ src/main/java/hu/nye/progtech/wumplus/service/command/impl/StepCommand.java
@@ -2,10 +2,12 @@ package hu.nye.progtech.wumplus.service.command.impl;
 
 import hu.nye.progtech.wumplus.model.CoordinateVO;
 import hu.nye.progtech.wumplus.model.GameState;
+import hu.nye.progtech.wumplus.model.PlayerVO;
 import hu.nye.progtech.wumplus.model.constants.CommandConst;
 import hu.nye.progtech.wumplus.model.constants.Element;
 import hu.nye.progtech.wumplus.service.command.Command;
 import hu.nye.progtech.wumplus.service.command.performer.StepPerformer;
+import hu.nye.progtech.wumplus.service.exception.PerformerException;
 import hu.nye.progtech.wumplus.service.util.MapQuery;
 
 public class StepCommand implements Command {
@@ -26,6 +28,16 @@ public class StepCommand implements Command {
 
     @Override
     public void process(String input) {
+        try {
+            PlayerVO stepedPlayer = stepPerformer.perform(gameState.getPlayerVO(), gameState.getMapVO());
+            gameState.setPlayerVO(stepedPlayer);
+        } catch (Exception e){
+            System.out.println("Can't perform this step");
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public GameState getGameState() {
+        return gameState;
     }
 }
--- src/test/hu/nye/progtech/wumplus/service/command/impl/StepCommandTest.java
+++ src/test/hu/nye/progtech/wumplus/service/command/impl/StepCommandTest.java
@@ -1,9 +1,14 @@
 package hu.nye.progtech.wumplus.service.command.impl;
 
+import hu.nye.progtech.wumplus.model.CoordinateVO;
 import hu.nye.progtech.wumplus.model.GameState;
 import hu.nye.progtech.wumplus.model.MapVO;
+import hu.nye.progtech.wumplus.model.PlayerVO;
 import hu.nye.progtech.wumplus.model.constants.CommandConst;
+import hu.nye.progtech.wumplus.model.constants.PlayerConst;
 import hu.nye.progtech.wumplus.service.command.performer.StepPerformer;
+import hu.nye.progtech.wumplus.service.exception.MapQueryException;
+import hu.nye.progtech.wumplus.service.exception.PerformerException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +18,12 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
 class StepCommandTest {
@@ -22,7 +33,7 @@ class StepCommandTest {
     @Mock
     private StepPerformer stepPerformerMock;
 
-    private GameState gameState = new GameState(null, null, false, false);
+    private GameState gameState = new GameState(null, new PlayerVO("teszt", PlayerConst.NORTH, new CoordinateVO(1,2)), false, false);
 
     private String STEP_CORRECT = CommandConst.STEP;
     private String STEP_WRONG = "LYJHCXF";
@@ -56,7 +67,46 @@ class StepCommandTest {
         Assertions.assertEquals(result, false);
     }
 
+
+
+
     @Test
-    void process() {
+    void processCorrect() throws PerformerException, MapQueryException {
+        System.out.println("[TEST\t] : Perform a correct step command");
+
+        // given
+        System.out.println("\t\t\tGIVEN\t:" + gameState);
+        System.out.println("\t\t\t\t\t:" + STEP_CORRECT);
+
+        PlayerVO PLAYER_STEPED = new PlayerVO("teszt", PlayerConst.NORTH, new CoordinateVO(1,1));
+        PLAYER_STEPED.setNonStatic(0,false,0,1);
+        given(stepPerformerMock.perform(any(), any())).willReturn(PLAYER_STEPED);
+
+        // when
+        underTest.process(STEP_CORRECT);
+        GameState expected = new GameState(null, PLAYER_STEPED, false, false);
+        System.out.println("\t\t\tWHEN\t:" + underTest.getGameState());
+        System.out.println("\t\t\t\t\t:" + expected);
+
+        // then
+        Assertions.assertEquals(expected, gameState);
+        verify(stepPerformerMock.perform(any(), any()));
+
+    }
+
+    @Test
+    void processWall() throws PerformerException, MapQueryException {
+        System.out.println("[TEST\t] : Try to perform a step to the wall");
+
+        // given
+        System.out.println("\t\t\tGIVEN\t:" + gameState);
+        System.out.println("\t\t\t\t\t:" + STEP_CORRECT);
+        
+        given(stepPerformerMock.perform(any(), any())).willThrow(new PerformerException("You can't step to the wall."));
+        // when
+        underTest.process(STEP_CORRECT);
+        GameState expected = new GameState(null, new PlayerVO("teszt", PlayerConst.NORTH, new CoordinateVO(1,2)), false, false);
+        // then
+        Assertions.assertEquals(expected, gameState);
     }
 }
