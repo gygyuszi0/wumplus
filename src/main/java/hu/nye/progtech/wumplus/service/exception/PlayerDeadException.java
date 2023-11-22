03d2773a8dcba5972347a14cb79541c657c7303f
gygyuszi0 <gyorgyigyula8@gmail.com>
2023-11-22 17:27:32 +0100

    Checkstyle

--- src/main/java/hu/nye/progtech/wumplus/service/exception/PlayerDeadException.java
+++ src/main/java/hu/nye/progtech/wumplus/service/exception/PlayerDeadException.java
@@ -1,5 +1,8 @@
 package hu.nye.progtech.wumplus.service.exception;
 
+/**
+ * Kivétel, ha a játékos meghal.
+ */
 public class PlayerDeadException extends Exception {
     public PlayerDeadException(String message) {
         super(message);
